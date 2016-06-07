ad778c762eb3be27feed1d064590812acedac9af
dtothefp <dtothefp@gmail.com>
2016-06-07 15:11:28 -0400

    Update 2016-06-07T19:11:27.555Z

--- README.md
+++
@@ -1,7 +0,0 @@
-#### Install and Run
-
-```
-nvm install 6 && npm i -g npm@latest
-npm i -g gulp-cli
-gulp watch
-```
--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-git config --global user.name "dtothefp"
-git config --global user.email "dtothefp@gmail.com"
-
-git remote rm origin
-git remote add origin https://dtothefp:${GITHUB_API_KEY}@github.com/dtothefp/nightwatch-nightmare.git
